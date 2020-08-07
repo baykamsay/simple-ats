496c1ff6f91225973b4a000d339555f0e233f4d1
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-08-07 11:40:41 +0300

    Added antd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import "../styles/antd.less";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
