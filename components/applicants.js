ac712d971054ea0c5473b4e8182b3b433453e7ba
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-08-14 17:45:40 +0300

    Updated applicants and job listings

--- components/applicants.js
+++ components/applicants.js
@@ -1,35 +1,40 @@
 import React from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Typography, Divider } from "antd";
 import styles from "../styles/ATS.module.css";
 
 const { Content, Sider } = Layout;
+const { Title } = Typography;
 
 export default function Applicants(props) {
-  console.log(props.data[0]);
   return (
     <Layout
       className={styles.siteLayoutBackground}
-      style={{ padding: "24px 0" }}
+      style={{ padding: "24px 0", minHeight: "81vh" }}
     >
-      <Sider className={styles.siteLayoutBackground} width={200}>
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={["0"]}
-          style={{ height: "100%" }}
+      <div style={{ padding: "0 24px" }}>
+        <Title level={2}>Applicants</Title>
+        <Divider />
+      </div>
+      <Layout className={styles.siteLayoutBackground}>
+        <Sider className={styles.siteLayoutBackground} width={200}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["0"]}
+            style={{ height: "100%" }}
+          >
+            {props.data.map((job, i) => (
+              <Menu.Item key={i.toString()}>{job.title}</Menu.Item>
+            ))}
+          </Menu>
+        </Sider>
+        <Content
+          style={{
+            padding: "0 24px",
+          }}
         >
-          {props.data.map((job, i) => (
-            <Menu.Item key={i.toString()}>{job.title}</Menu.Item>
-          ))}
-        </Menu>
-      </Sider>
-      <Content
-        style={{
-          padding: "0 24px",
-          minHeight: "76vh",
-        }}
-      >
-        Content
-      </Content>
+          Content
+        </Content>
+      </Layout>
     </Layout>
   );
 }
--- components/jobListings.js
+++ components/jobListings.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { Layout } from "antd";
+import {
+  Layout,
+  List,
+  Card,
+  Typography,
+  Divider,
+  Button,
+  Row,
+  Col,
+} from "antd";
 import styles from "../styles/ATS.module.css";
+import { PlusOutlined } from "@ant-design/icons";
 
 const { Content } = Layout;
+const { Title } = Typography;
 
-export default function JobListings() {
+export default function JobListings(props) {
   return (
     <Layout
       className={styles.siteLayoutBackground}
@@ -16,7 +27,44 @@ export default function JobListings() {
           minHeight: "76vh",
         }}
       >
-        Content
+        <Row align="middle">
+          <Col flex="auto">
+            <Title style={{ margin: 0 }} level={2}>
+              Job Listings
+            </Title>
+          </Col>
+          <Col>
+            <Button type="primary" icon={<PlusOutlined />} size="large">
+              Add Listing
+            </Button>
+          </Col>
+        </Row>
+        <Divider />
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 2,
+            md: 4,
+            lg: 4,
+            xl: 6,
+            xxl: 3,
+          }}
+          dataSource={props.data}
+          renderItem={(item) => (
+            <List.Item>
+              <Card
+                hoverable
+                onClick={() => {
+                  console.log("hey"); // change here
+                }}
+                title={item.title}
+              >
+                {item.description}
+              </Card>
+            </List.Item>
+          )}
+        />
       </Content>
     </Layout>
   );
