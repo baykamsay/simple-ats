6cdae1e4116f4904b22436fb94ed0e00f225bdcd
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-08-28 00:54:55 +0300

    Updated jobs page

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,130 @@
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
-import { Typography } from "antd";
-
-const { Title } = Typography;
+import listingStyles from "../styles/ListingsPage.module.css";
+import { Row, Col, Button } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} {...listingStyles}>
       <Head>
         <title>Home</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main className={styles.main}>
-        <Title>
-          Go to{" "}
+        <Row
+          style={{
+            minHeight: "60vh",
+          }}
+          className={listingStyles.secondaryB}
+          justify="center"
+          align="bottom"
+        >
+          <Col
+            xs={{ span: 20 }}
+            lg={{ span: 8 }}
+            style={{ paddingBottom: "5rem" }}
+          >
+            <h1 className={listingStyles.primary}>Example Jobs</h1>
+            <h5 className={listingStyles.primary}>
+              Join Us &ndash; add small and meaningful slogan here
+            </h5>
+            <Link href="/ats">
+              <a className={listingStyles.button}>View Open Jobs</a>
+            </Link>
+          </Col>
+          <Col
+            xs={{ span: 20 }}
+            lg={{ span: 8 }}
+            style={{ paddingBottom: "5rem" }}
+          >
+            <p className={listingStyles.primary}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              interdum hendrerit pulvinar. Cras accumsan imperdiet augue in
+              ornare. Ut ac ex eu leo aliquet porttitor ac id ligula.
+            </p>
+            <p
+              className={[
+                listingStyles.primary,
+                listingStyles.marginBottom,
+              ].join(" ")}
+            >
+              Nulla pellentesque lorem turpis, in rhoncus urna euismod sit amet.
+              Aenean pharetra justo felis, quis congue dui faucibus non. Nulla
+              blandit lacinia dolor, sit amet mattis massa fermentum ac.in.
+            </p>
+          </Col>
+        </Row>
+        <Row style={{ minHeight: "30vh", paddingTop: "5%" }} justify="center">
+          <Col xs={{ span: 20 }} lg={{ span: 8 }}>
+            <h2>Our Mission</h2>
+          </Col>
+          <Col xs={{ span: 20 }} lg={{ span: 8 }}>
+            <p
+              className={[listingStyles.mission, listingStyles.secondary].join(
+                " "
+              )}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              interdum hendrerit pulvinar. Cras accumsan imperdiet augue in
+              ornare.
+            </p>
+          </Col>
+        </Row>
+        <Row
+          style={{ minHeight: "10vh", paddingBottom: "1rem" }}
+          justify="center"
+          align="bottom"
+        >
+          <DownOutlined style={{ color: "black", fontSize: "1.5rem" }} />
+        </Row>
+        <Row style={{ paddingTop: "5rem" }} justify="center" align="bottom">
+          <Col
+            style={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+            xs={{ span: 20 }}
+            lg={{ span: 16 }}
+          >
+            <h2>Our Team</h2>
+            <Col
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <img src="/our-team.png" alt="Our Team" width="100%"></img>
+              <span>
+                Photo by{" "}
+                <a href="https://unsplash.com/@annaelizaearl?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                  Anna Earl
+                </a>{" "}
+                on{" "}
+                <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
+                  Unsplash
+                </a>
+              </span>
+            </Col>
+          </Col>
+        </Row>
+        <Row style={{ padding: "4rem 0" }} justify="center" align="bottom">
           <Link href="/ats">
-            <a>the ATS</a>
+            <a className={listingStyles.buttonS}>Join Us</a>
           </Link>
-        </Title>
+        </Row>
       </main>
 
       <footer className={styles.footer}>
-        <a href="https://baykam.me" target="_blank" rel="noopener noreferrer">
-          Powered by Baykam Say
+        <a
+          href="https://github.com/baykamsay/simple-ats"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View on GitHub
         </a>
       </footer>
     </div>
