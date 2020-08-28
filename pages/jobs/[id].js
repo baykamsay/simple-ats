2c778c189bf965e02695e2695805d42092070a8f
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-08-28 14:39:59 +0300

    Added listing pages

--- pages/jobs/[id].js
+++ pages/jobs/[id].js
@@ -1,13 +1,30 @@
 import Head from "next/head";
-import Link from "next/link";
 import styles from "../../styles/Home.module.css";
 import listingStyles from "../../styles/ListingsPage.module.css";
-import { Row, Col, Divider } from "antd";
+import { Row, Col, Spin } from "antd";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+import fetch from "isomorphic-unfetch";
+import ReactMarkdown from "react-markdown";
 
 function Jobs() {
   const router = useRouter();
   const { id } = router.query;
+
+  const { data, error } = useSWR(`/api/jobListing/${id}`, async function (
+    args
+  ) {
+    const res = await fetch(args);
+    return res.json();
+  });
+  if (error) return <div>failed to load</div>;
+  if (!data)
+    return (
+      <div className={styles.container}>
+        <Spin size="large" />
+      </div>
+    );
+  console.log(data);
   return (
     <div className={styles.container} {...listingStyles}>
       <Head>
@@ -18,7 +35,9 @@ function Jobs() {
       <main className={styles.main}>
         <Row style={{ paddingTop: "4rem" }} justify="center" align="top">
           <Col xs={{ span: 20 }} lg={{ span: 12 }}>
-            <h1>{id}</h1>
+            <h1>{data.title}</h1>
+            <h5>{data.location}</h5>
+            <ReactMarkdown source={data.description} />
           </Col>
         </Row>
       </main>
@@ -35,16 +54,5 @@ function Jobs() {
     </div>
   );
 }
-
-// export async function getStaticProps() {
-//   const res = await fetch("http://localhost:3000/api/jobs");
-//   const jobs = await res.json();
-
-//   return {
-//     props: {
-//       jobs,
-//     },
-//   };
-// }
 
 export default Jobs;
